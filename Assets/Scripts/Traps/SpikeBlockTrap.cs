7f8a664e3950270cdd0cb177c7771f9653ca28d5
Lycoris Radiata <lycoris.radiata@mofumofustudios.com>
2022-12-27 12:19:30 +0100

    Fix spike block trap (would push colliding objects into the floor)

--- Assets/Scripts/Traps/SpikeBlockTrap.cs
+++ Assets/Scripts/Traps/SpikeBlockTrap.cs
@@ -6,14 +6,34 @@ public class SpikeBlockTrap : MonoBehaviour
 {
     private static AudioManager am;
     private static PlayerHealthManager playerHealthManager;
-    private Rigidbody rb;
-    private bool isActivated;
+    private bool isActivated, isColliding;
+    private float targetPositionY;
 
     void Awake()
     {
         am = FindObjectOfType<AudioManager>();
         playerHealthManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthManager>();
-        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        isActivated = false;
+        isColliding = false;
+        targetPositionY = transform.position.y - 4f;
+    }
+
+    void FixedUpdate()
+    {
+        if (isActivated && !isColliding)
+        {
+            if (transform.position.y > targetPositionY)
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z);
+            else
+            {
+                isActivated = false;
+                isColliding = true;
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -22,14 +42,20 @@ public class SpikeBlockTrap : MonoBehaviour
         {
             am.Play("TrapTriggered");
             am.Play("SpikeTrapMovement");
-            rb.isKinematic = false;
             isActivated = true;
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        isColliding = true;
+
         if (collision.gameObject.CompareTag("Player"))
             playerHealthManager.RemoveLifePoints(50);
+    }
+
+    void OnCollisionExit()
+    {
+        isColliding = false;
     }
 }
